729c62f261964e25ac5fcfc48de8000283aeb161
wuliqun <1161913566@qq.com>
2022-03-30 16:56:04 +0800

    1

--- src/components/BaseLayout/index.vue
+++ src/components/BaseLayout/index.vue
@@ -2,14 +2,17 @@
     <div class="baseLayout">
         <top-head />
         <router-view />
+        <bottom-head />
     </div>
 </template>
 
 <script>
 import header from '@/components/Header';
+import bottom from '@/components/Bottom';
 export default {
     components: {
-        'top-head': header
+        'top-head': header,
+        'bottom-head': bottom
     }
 };
 </script>
